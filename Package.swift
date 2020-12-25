72abb834e4fcc3e1356c9a3b2bac00c2058d8f76
Dmitrii <zeionara@gmail.com>
2020-12-25 18:21:46 +0300

    Updated dependencies

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.1
+// swift-tools-version:5.3
 // Generated automatically by Perfect Assistant Application
 // Date: 2017-09-20 19:18:02 +0000
 import PackageDescription
@@ -9,9 +9,9 @@ let package = Package(
 	],
 	products: [.library(name: "PerfectCURL", targets: ["PerfectCURL"])],
 	dependencies: [
-		.package(url: "https://github.com/PerfectlySoft/Perfect-libcurl.git", from: "2.0.0"),
+		.package(name: "cURL", url: "https://github.com/PerfectlySoft/Perfect-libcurl.git", from: "2.0.0"),
 		.package(url: "https://github.com/PerfectlySoft/PerfectLib.git", from: "4.0.0"),
-		.package(url: "https://github.com/PerfectlySoft/Perfect-Thread.git", from: "3.0.0"),
+		.package(name: "PerfectThread", url: "https://github.com/PerfectlySoft/Perfect-Thread.git", from: "3.0.0"),
 	],
 	targets: [
 		.target(name: "PerfectCURL", dependencies: ["PerfectLib", "PerfectThread"]),
